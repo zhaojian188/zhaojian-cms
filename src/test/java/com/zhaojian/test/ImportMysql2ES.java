cbe86d8f972eda05377d19a2cdcb2021301c6cbe
zhaojian188 <1294626274@qq.com>
2019-12-19 09:42:42 +0800

    添加测试类中用es添加数据解决依赖冲突的注释==>12-19 9:40

--- src/test/java/com/zhaojian/test/ImportMysql2ES.java
+++ src/test/java/com/zhaojian/test/ImportMysql2ES.java
@@ -38,7 +38,12 @@ public class ImportMysql2ES {
 	//注入es仓库，实现简单的增删改查操作
 	@Autowired
 	ArticleReposit articleReposit;
-	
+	//冲突解决方案:
+	//1.java.lang.ClassNotFoundException: com.fasterxml.jackson.core.type.ResolvedType
+	// 解决:找到pom文件,粘贴上<!-- 解决冲突的依赖 -->,等待加载完毕,就ok了
+	//2.到pom文件里修改jetty版本为:9.4.9.v20180320
+	//3.到pom文件里修改validate版本
+	//    <validator.version>5.1.0.Final</validator.version>
 	
 	@Test
 	public void testImport() {
