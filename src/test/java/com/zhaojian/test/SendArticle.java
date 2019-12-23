3952daab5333860e5ce76441163084c75ad79bf4
zhaojian188 <1294626274@qq.com>
2019-12-23 19:30:59 +0800

    更新 测试添加1000篇文章到kafka的测试类-SendArticle

--- src/test/java/com/zhaojian/test/SendArticle.java
+++ src/test/java/com/zhaojian/test/SendArticle.java
@@ -42,42 +42,48 @@ public class SendArticle {
 	@Test
 	public void test() throws Exception {
 		
-		
+		//获取放文章的文件夹的路径
 		File file = new File("D:/1707D");
-		//获取文件夹的每篇文章
+		//遍历这个文件夹获取文件夹的每篇文章
 		File[] listFiles = file.listFiles();
+		//循环，获取每篇文章
 		for (File file2 : listFiles) {
-			//标题
+			//标题，把文章名中的.txt，替换成""，做文章标题
 			String title = file2.getName().replace(".txt", "");
-			//内容
+			//使用工具类读取文章内容，做内容
 			String content = FileUtils.readFileString(file2, "UTF-8");
-			
+			//声明Article对象
 			Article article = new Article();
+			//把属性赋值给对象
+			//标题
 			article.setTitle(title);
+			//内容
 			article.setContent(content);
+			//摘要，如果大于140个字，则截取前140个字作为摘要，否则，有多少个字，截取多少个字作为摘要
 			if(content.length()>140){
 				article.setDigest(content.substring(0, 139));
 			} else {
 				article.setDigest(content.substring(0, content.length()));
 			}
 			//生成1-8的随机频道id
-			int[] randomForCMS = RandomUtils.getRandomForCMS(9);
+			int[] randomForCMS = RandomUtils.getRandomToCMS(9);
+			//生成频道id
 			article.setChannelId(randomForCMS[0]);
+			//生成分类id
 			article.setCategoryId(randomForCMS[1]);
 			//普通用户的id
 			article.setUserId(50);
 			//点击量
 			article.setHits(0);
-			//设置热门
+			//设置热门 0,1随机
 			article.setHot(RandomUtils.getRandomInt(0, 1));
-			//审核
+			//审核 0,1随机
 			article.setStatus(RandomUtils.getRandomInt(0, 1));
 				
 			article.setDeleted(0);
 			String jsonString = JSON.toJSONString(article);
-			//kafka发消息
+			//向kafka消费者发消息
 			kafkaTemplate.send("articles",jsonString);
-			
 		}
 	}
 	
